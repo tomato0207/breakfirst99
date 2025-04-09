8017b31adc13be7456ed81d4bebc04f043a0f722
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-04-09 17:58:45 +0800

    test

--- app/home2/page.jsx
+++ app/home2/page.jsx
@@ -1,8 +1,7 @@
 import homeImg from "@/public/food01.jpg";
 import Hero from "@/app/components/hero";
 
-export default function Home2Page({
-params }) {
+export default function Home2Page({params }) {
     return <Hero imgUrl={homeImg} content={"Do My Best!"}/>;
     }
     
