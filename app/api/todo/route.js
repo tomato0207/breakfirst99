edc92152273b23695bf8b974bf30d976dee6dd1f
tomato0207 <frankttps@gmail.com>
2025-04-30 17:48:10 +0800

    0430

---
+++ app/api/todo/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+export async function GET(request){
+    let todos = await prisma.todo.findMany({orderBy:[{id:"asc"}]});
+    let queryStr = request.nextUrl.searchParams.get("filter");
+    if (!queryStr){
+         return NextResponse.json(todos);
+    }
+
+    queryStr=queryStr.toLowerCase();
+    todos = todos.filter((todo)=>todo.title.includes(queryStr));
+    if (todos.length==0){
+        return NextResponse.error(new Error(`No todos with ${queryStr}`),
+    {
+        status:404,
+    });
+    }
+    return NextResponse.json(todos);
+}
+
+export async function POST(request){
+    const {title} = await request.json();
+    try{
+        const todo= await prisma.todo.create({
+            data:{title},
+        });
+        return NextResponse.json(todo);
+    }catch(error){
+        return NextResponse.json(new Error("Error creating todo"),{
+            status:500,
+        });
+    }
+}
